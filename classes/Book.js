f60a8563914ee9b190b310bdf14fe149a418387f
JRSkates <skatesproduction@gmail.com>
2024-10-22 15:10:40 +0100

    Book class created

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,29 @@
 // import the Media class:
-
+const Media = require('./Media.js')
 // create your Book class:
+class Book extends Media {
+    constructor(title, genre, year, author, numPages, rating) {
+        super(title, genre, year)
+        this.author = author
+        this.numPages = numPages
+        this.rating = rating
+    }
 
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+
+    static highestRating(arr) {
+        let highest = null
+        
+        arr.forEach(book => {
+            if (!highest || book.rating > highest.rating) {
+                highest = book
+            }
+        })
+
+        return highest
+    }
+}
 // don't change below
 module.exports = Book;
--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 // Import classes here to console.log and debug
 const Media = require('./Media.js');
+const Book = require('./Book.js');
